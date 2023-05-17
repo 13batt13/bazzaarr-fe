57a61790da8e04c0848e738cbb999c54796916e0
13batt13 <13batt13@gmail.com>
2023-05-17 17:17:34 +0300

    setupExitSignals false

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,8 +4,9 @@ import { BuildOptions } from "./types/config";
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
-    open: true,
+    open: false,
     historyApiFallback: true,
     hot: true,
+    setupExitSignals: false,
   };
 }
